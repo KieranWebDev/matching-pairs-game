c6510a9b46239d79ce31582c47521d6ccb023c72
Kieran <kieran.webdevelopment@gmail.com>
2022-07-05 07:01:03 +0100

    modal and children props tutorial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Modal from './components/Modal';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+      <Modal>
+        <h2>10% off coupon code!</h2>
+        <p>use the code NINJA at checkout</p>
+      </Modal>
     </div>
   );
 }
