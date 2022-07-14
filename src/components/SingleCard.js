88858f4b0e29dae60a478fa9feee26f2f5a11a12
Kieran <kieran.webdevelopment@gmail.com>
2022-07-14 07:16:02 +0100

    added state and functions to handle player choices

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,12 +1,21 @@
 import './SingleCard.css';
 
-export default function SingleCard({ card }) {
+export default function SingleCard({ card, handleChoice }) {
+  function handleClick() {
+    handleChoice(card);
+  }
+
   return (
     <>
       <div className="card" key={card.id}>
         <div>
           <img className="front" src={card.src} alt="card-front" />
-          <img className="back" src="/img/cover.png" alt="card-back" />
+          <img
+            className="back"
+            src="/img/cover.png"
+            alt="card-back"
+            onClick={handleClick}
+          />
         </div>
       </div>
     </>
